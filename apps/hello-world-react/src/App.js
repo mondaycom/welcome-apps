283f099d861100656fc007c74396b3b184df5b7e
Ben Yitzhaki <beny@monday.com>
2020-03-22 16:48:54 +0200

    add username to hello world react

--- apps/hello-world-react/src/App.js
+++ apps/hello-world-react/src/App.js
@@ -5,12 +5,13 @@ const monday = mondaySdk();
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { settings: {}, context: {} };
+    this.state = { settings: {}, context: {}, userName: "monday" };
   }
 
   componentDidMount() {
     monday.listen("settings", this.getSettings);
     monday.listen("context", this.getContext);
+    monday.api(`query { me { name } }`).then(this.getUser)
   }
 
   getSettings = res => {
@@ -21,12 +22,16 @@ class App extends React.Component {
     this.setState({ context: res.data });
   };
 
+  getUser = res => {
+    this.setState({ userName: res.data.me.name });
+  };
+
   render() {
-    const { settings, context } = this.state;
+    const { settings, context, userName } = this.state;
 
     return (
       <div className="monday-app">
-        <div>Hello monday!</div>
+        <div>Hello {userName}!</div>
         <div>Your context: {JSON.stringify(context)}</div>
         <div>Your settings: {JSON.stringify(settings)}</div>
       </div>
