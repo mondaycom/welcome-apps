76f86051614eb554380c417a39acd5b885e408d2
Ofir Geller <ofirge@monday.com>
2020-12-20 09:31:14 +0200

    add quickstart-react-ts template

---
+++ apps/quickstart-react-ts/src/middlewares/authentication.ts
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+const SHARED_SECRET = process.env.MONDAY_SIGNING_SECRET;
+
+export default async function authenticationMiddleware(req, res, next) {
+  try {
+    let { authorization } = req.headers;
+    if (!authorization && req.query) {
+      authorization = req.query.token;
+    }
+    const { accountId, userId, backToUrl, shortLivedToken } = jwt.verify(
+      authorization,
+      process.env.MONDAY_SIGNING_SECRET
+    );
+    req.session = { accountId, userId, backToUrl, shortLivedToken };
+    next();
+  } catch (err) {
+    res.status(500).json({ error: 'not authenticated' });
+  }
+}
