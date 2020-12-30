782e06057ab2f5bf3b4ab8eeec0447c4a39b87bb
Vlad Mystetskyi <62242004+vladmonday@users.noreply.github.com>
2020-12-30 20:20:04 +0200

    fix http to https

--- apps/quickstart-react/src/App.js
+++ apps/quickstart-react/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import mondaySdk from "monday-sdk-js";
 import "monday-ui-react-core/dist/main.css"
-//Explore more Monday React Components here: http://style.monday.com/
+//Explore more Monday React Components here: https://style.monday.com/
 import AttentionBox from "monday-ui-react-core/dist/AttentionBox.js"
 
 const monday = mondaySdk();
