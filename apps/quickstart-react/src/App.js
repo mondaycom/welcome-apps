c635eae9976b07d0d0bbbdef2ca9f1a77cc91728
NadavAvraham <116634440+NadavAvraham@users.noreply.github.com>
2022-11-29 21:00:45 +0200

    Added example of monday.execute

--- apps/quickstart-react/src/App.js
+++ apps/quickstart-react/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import mondaySdk from "monday-sdk-js";
-import "monday-ui-react-core/dist/main.css"
+import "monday-ui-react-core/dist/main.css";
 //Explore more Monday React Components here: https://style.monday.com/
-import AttentionBox from "monday-ui-react-core/dist/AttentionBox.js"
+import AttentionBox from "monday-ui-react-core/dist/AttentionBox.js";
 
+// Usage of mondaySDK example, for more information visit here: https://developer.monday.com/apps/docs/introduction-to-the-sdk/
 const monday = mondaySdk();
+const apiAccessToken = "Enter your access token here";
+monday.setToken(apiAccessToken);
+monday.get("context").then((res) => console.log(res));
 
 class App extends React.Component {
   constructor(props) {
@@ -19,17 +23,23 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    // Notice this method notifies the monday platform that user gains a first value in an app.
+    // Read more about it here: https://developer.monday.com/apps/docs/mondayexecute#value-created-for-user/
+    monday.execute("valueCreatedForUser");
+
     // TODO: set up event listeners
   }
 
   render() {
-    return <div className="App">
-      <AttentionBox
-        title="Hello Monday Apps!"
-        text="Let's start building your amazing app, which will change the world!"
-        type="success"
-      />
-    </div>;
+    return (
+      <div className="App">
+        <AttentionBox
+          title="Hello Monday Apps!"
+          text="Let's start building your amazing app, which will change the world!"
+          type="success"
+        />
+      </div>
+    );
   }
 }
 
