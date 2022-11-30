ba8c99284dd71a18e2338f39cf07aa28ae2827fe
NadavAvraham <nadavav@monday.com>
2022-11-30 17:33:52 +0200

    setup new template for react quickstart

--- apps/quickstart-react/src/App.js
+++ apps/quickstart-react/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import mondaySdk from "monday-sdk-js";
 import "monday-ui-react-core/dist/main.css";
@@ -7,40 +8,36 @@ import AttentionBox from "monday-ui-react-core/dist/AttentionBox.js";
 
 // Usage of mondaySDK example, for more information visit here: https://developer.monday.com/apps/docs/introduction-to-the-sdk/
 const monday = mondaySdk();
-const apiAccessToken = "Enter your access token here";
-monday.setToken(apiAccessToken);
-monday.get("context").then((res) => console.log(res));
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [context, setContext] = useState();
 
-    // Default state
-    this.state = {
-      settings: {},
-      name: "",
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     // Notice this method notifies the monday platform that user gains a first value in an app.
     // Read more about it here: https://developer.monday.com/apps/docs/mondayexecute#value-created-for-user/
     monday.execute("valueCreatedForUser");
 
-    // TODO: set up event listeners
-  }
+    // TODO: set up event listeners, Here`s an example, read more here: https://developer.monday.com/apps/docs/mondaylisten/
+    monday.listen("context", (res) => {
+      setContext(res.data);
+    });
+  }, []);
 
-  render() {
-    return (
-      <div className="App">
-        <AttentionBox
-          title="Hello Monday Apps!"
-          text="Let's start building your amazing app, which will change the world!"
-          type="success"
-        />
-      </div>
-    );
-  }
-}
+  //Some example what you can do with context, read more here: https://developer.monday.com/apps/docs/mondayget#requesting-context-and-settings-data
+  const attentionBoxText = `Hello, your user_id is: ${
+    context ? context.user.id : "still loading"
+  }.
+  Let's start building your amazing app, which will change the world!`;
+
+  return (
+    <div className="App">
+      <AttentionBox
+        title="Hello Monday Apps!"
+        text={attentionBoxText}
+        type="success"
+      />
+    </div>
+  );
+};
 
 export default App;
