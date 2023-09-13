5b9460c3222f07ea1ce0fe619490a3900579d917
rami-monday <rami@monday.com>
2023-09-13 17:11:32 +0300

    created workpsace app view template app

---
+++ apps/workspace-view-app/src/components/boards/board-card/board-card-component.jsx
@@ -0,0 +1,50 @@
+import { Flex, Heading } from "monday-ui-react-core";
+import React from "react";
+import classes from "./board-card-component.module.scss";
+import { getAccountDetails } from "../../../services/hooks";
+
+const BoardCardComponent = ({ board }) => {
+  const onClick = async () => {
+    const { baseUrl } = await getAccountDetails();
+    window.open(`${baseUrl}boards/${board.id}`);
+  };
+  return (
+    <Flex
+      className={classes.boardCardComponent}
+      direction={Flex.directions.COLUMN}
+      align={Flex.align.CENTER}
+      justify={Flex.justify.CENTER}
+      onClick={onClick}
+    >
+      <Heading
+        value={board.name}
+        type={Heading.types.h2}
+        ellipsisMaxLines={2}
+      />
+      <Heading
+        value={board.description}
+        type={Heading.types.h3}
+        ellipsisMaxLines={4}
+      />
+      <Flex
+        className={classes.footer}
+        direction={Flex.directions.ROW}
+        align={Flex.align.CENTER}
+        justify={Flex.justify.START}
+        gap={12}
+      >
+        <img
+          className={classes.creatorPhoto}
+          src={board.creator?.photo_thumb}
+          alt="owner"
+        />
+        <Heading
+          value={"Created by " + board.creator?.name}
+          type={Heading.types.h6}
+        />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default BoardCardComponent;
