0a12e12043d4dbaa96ebfe964a4892fb3f048534
Maor Barazani <maorba@monday.com>
2024-04-28 11:42:41 +0300

    add env output to  /health

--- apps/quickstart-integrations-monday-code/index.js
+++ apps/quickstart-integrations-monday-code/index.js
@@ -1,19 +1,19 @@
-import express from "express";
-import { Logger } from "@mondaycom/apps-sdk";
-import { transformText } from "./src/transformation-service.js";
-import { authorizeRequest } from "./src/middleware.js";
-import { changeColumnValue, getColumnValue } from "./src/monday-api-service.js";
-import { getSecret, isDevelopmentEnv, getEnv } from "./src/helpers.js";
-import dotenv from "dotenv";
-import { readQueueMessage, produceMessage } from "./src/queue-service.js";
-import { SecretsManager } from "@mondaycom/apps-sdk";
+import express from 'express';
+import { Logger, SecretsManager } from '@mondaycom/apps-sdk';
+import { transformText } from './src/transformation-service.js';
+import { authorizeRequest } from './src/middleware.js';
+import { changeColumnValue, getColumnValue } from './src/monday-api-service.js';
+import { getEnv, getSecret, isDevelopmentEnv } from './src/helpers.js';
+import dotenv from 'dotenv';
+import { produceMessage, readQueueMessage } from './src/queue-service.js';
+
 dotenv.config();
 
-const logTag = "ExpressServer";
-const PORT = "PORT";
-const SERVICE_TAG_URL = "SERVICE_TAG_URL";
-const TO_UPPER_CASE = "TO_UPPER_CASE";
-const TO_LOWER_CASE = "TO_LOWER_CASE";
+const logTag = 'ExpressServer';
+const PORT = 'PORT';
+const SERVICE_TAG_URL = 'SERVICE_TAG_URL';
+const TO_UPPER_CASE = 'TO_UPPER_CASE';
+const TO_LOWER_CASE = 'TO_LOWER_CASE';
 
 const logger = new Logger(logTag);
 const currentPort = getSecret(PORT); // Port must be 8080 to work with monday code
@@ -22,7 +22,7 @@ const currentUrl = getSecret(SERVICE_TAG_URL);
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const secrets = new SecretsManager();
   let secretsObject = {};
   secrets.getKeys().forEach((key) => {
@@ -30,16 +30,29 @@ app.get("/", (req, res) => {
     logger.info(`Secret key: ${key}, value: ${secrets.get(key)}`);
   });
 
-  res.status(200).send(secretsObject);
+  const envs = new SecretsManager();
+  let envsObject = {};
+  envs.getKeys().forEach((key) => {
+    envsObject[key] = envs.get(key);
+    logger.info(`Env key: ${key}, value: ${envs.get(key)}`);
+  });
+
+  let processEnv = {};
+  try {
+    processEnv = JSON.parse(process.env);
+  } catch (e) {
+    logger.error(`Error parsing process.env: ${e}`);
+  }
+  res.status(200).send({ secretsObject, envsObject, processEnv });
 });
 
-app.post("/monday/execute_action", authorizeRequest, async (req, res) => {
+app.post('/monday/execute_action', authorizeRequest, async (req, res) => {
   logger.info(
     JSON.stringify({
-      message: "New request received",
-      path: "/action",
+      message: 'New request received',
+      path: '/action',
       body: req.body,
-      headers: req.headers,
+      headers: req.headers
     })
   );
   const { shortLivedToken } = req.session;
@@ -52,7 +65,7 @@ app.post("/monday/execute_action", authorizeRequest, async (req, res) => {
       itemId,
       sourceColumnId,
       targetColumnId,
-      transformationType,
+      transformationType
     } = inputFields;
 
     const text = await getColumnValue(shortLivedToken, itemId, sourceColumnId);
@@ -61,7 +74,7 @@ app.post("/monday/execute_action", authorizeRequest, async (req, res) => {
     }
     const transformedText = transformText(
       text,
-      transformationType ? transformationType.value : "TO_UPPER_CASE"
+      transformationType ? transformationType.value : 'TO_UPPER_CASE'
     );
 
     await changeColumnValue(
@@ -75,55 +88,55 @@ app.post("/monday/execute_action", authorizeRequest, async (req, res) => {
     return res.status(200).send({});
   } catch (err) {
     logger.error(err);
-    return res.status(500).send({ message: "internal server error" });
+    return res.status(500).send({ message: 'internal server error' });
   }
 });
 
 app.post(
-  "/monday/get_remote_list_options",
+  '/monday/get_remote_list_options',
   authorizeRequest,
   async (req, res) => {
     const TRANSFORMATION_TYPES = [
-      { title: "to upper case", value: TO_UPPER_CASE },
-      { title: "to lower case", value: TO_LOWER_CASE },
+      { title: 'to upper case', value: TO_UPPER_CASE },
+      { title: 'to lower case', value: TO_LOWER_CASE }
     ];
     try {
       return res.status(200).send(TRANSFORMATION_TYPES);
     } catch (err) {
       logger.error(err);
-      return res.status(500).send({ message: "internal server error" });
+      return res.status(500).send({ message: 'internal server error' });
     }
   }
 );
 
 
 app.post(
-    "/produce",
-    async (req, res) => {
-        try {
-            const { body } = req;
-            const message = JSON.stringify(body);
-            const messageId = await produceMessage(message);
-            return res.status(200).send({ messageId });
-        } catch (err) {
-            logger.error(JSON.stringify(err));
-            return res.status(500).send({ message: "internal server error" });
-        }
+  '/produce',
+  async (req, res) => {
+    try {
+      const { body } = req;
+      const message = JSON.stringify(body);
+      const messageId = await produceMessage(message);
+      return res.status(200).send({ messageId });
+    } catch (err) {
+      logger.error(JSON.stringify(err));
+      return res.status(500).send({ message: 'internal server error' });
     }
+  }
 );
 
 app.post(
-    "/mndy-queue",
-    async (req, res) => {
-        try {
-            const { body, query } = req;
-            readQueueMessage({ body, query });
-            return res.status(200).send({}); // return 200 to ACK the queue message
-        } catch (err) {
-            logger.error(err.error);
-            return res.status(500).send({ message: "internal server error" });
-        }
+  '/mndy-queue',
+  async (req, res) => {
+    try {
+      const { body, query } = req;
+      readQueueMessage({ body, query });
+      return res.status(200).send({}); // return 200 to ACK the queue message
+    } catch (err) {
+      logger.error(err.error);
+      return res.status(500).send({ message: 'internal server error' });
     }
+  }
 );
 
 app.listen(currentPort, () => {
@@ -132,11 +145,11 @@ app.listen(currentPort, () => {
   } else {
     logger.info(
       `up and running listening on port:${currentPort}`,
-      "server_runner",
+      'server_runner',
       {
         env: getEnv(),
         port: currentPort,
-        url: `https://${currentUrl}`,
+        url: `https://${currentUrl}`
       }
     );
   }
