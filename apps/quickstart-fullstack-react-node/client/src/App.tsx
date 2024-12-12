3399eeeaf1e0b5720303c459d41da8077b1c55c3
Roy Weisselberg <roywe@monday.com>
2024-12-12 16:04:40 +0200

    fixed PR comments

--- apps/quickstart-fullstack-react-node/client/src/App.tsx
+++ apps/quickstart-fullstack-react-node/client/src/App.tsx
@@ -1,21 +1,19 @@
-import React from "react";
-import {useState, useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 import mondaySdk, {MondayClientSdk} from "monday-sdk-js";
 import "monday-ui-react-core/dist/main.css";
-//Explore more Monday React Components here: https://style.monday.com/
+//Explore more Monday React Components here: https://vibe.monday.com/
 import AttentionBox from "monday-ui-react-core/dist/AttentionBox.js";
 import {TextField} from "@vibe/core";
 
 // Usage of mondaySDK example, for more information visit here: https://developer.monday.com/apps/docs/introduction-to-the-sdk/
 const monday: MondayClientSdk = mondaySdk();
+const STORAGE_EXAMPLE_KEY = 'my_example_key';
 
 const App = () => {
     const [context, setContext] = useState<Object>();
-    const [text, setText] = useState<String>();
+    const [storageExampleText, setStorageExampleText] = useState<String>();
 
-
-    const my_key = 'aa';
     useEffect(() => {
         // Notice this method notifies the monday platform that user gains a first value in an app.
         // Read more about it here: https://developer.monday.com/apps/docs/mondayexecute#value-created-for-user/
@@ -26,25 +24,35 @@ const App = () => {
             setContext(res.data);
         });
 
-        monday.storage.instance.getItem(my_key).then(res => {
-            setText(res.data.value);
+        monday.storage.instance.getItem(STORAGE_EXAMPLE_KEY).then(res => {
+            setStorageExampleText(res.data.value);
         })
     }, []);
 
     useEffect(() => {
-        if (text !== undefined) {
-            monday.storage.instance.setItem(my_key, text)
+        if (storageExampleText !== undefined) {
+            monday.storage.instance.setItem(STORAGE_EXAMPLE_KEY, storageExampleText)
         }
-    }, [text]);
+    }, [storageExampleText]);
 
     //Some example what you can do with context, read more here: https://developer.monday.com/apps/docs/mondayget#requesting-context-and-settings-data
-    let attentionBoxText: String = `Could not load context data`
-    let attentionBoxType: String = 'warning'
 
-    if (context) {
-        attentionBoxText = `Hello, your user_id is: ${context.user.id}`;
-        attentionBoxType = 'success';
-    }
+    const getAttentionBox = () => {
+        if (context) {
+            return {
+                attentionBoxText: `Hello, your user_id is: ${context.user.id}`,
+                attentionBoxType: 'success'
+            }
+        } else
+        {
+            return {
+                attentionBoxText: `Could not load context data`,
+                attentionBoxType: 'warning'
+            }
+        }
+    };
+
+    const { attentionBoxText, attentionBoxType} = getAttentionBox()
 
     return (
         <div className="App">
@@ -53,8 +61,8 @@ const App = () => {
                 text={attentionBoxText}
                 type={attentionBoxType}
             />
-            <TextField title="My first text field" value={text} onChange={async (value: String, event) => {
-                setText(value)
+            <TextField title="My first text field" value={storageExampleText} onChange={async (value: String, event) => {
+                setStorageExampleText(value)
             }}></TextField>
 
         </div>
