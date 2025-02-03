165788c35eb7c2f3d8dd15d6633da3c0b52a03ad
Dipro B <dipro@monday.com>
2025-02-03 12:45:37 -0500

    added comment about authenticating incoming requests

--- apps/github-monday-code/src/routes/trigger.js
+++ apps/github-monday-code/src/routes/trigger.js
@@ -14,7 +14,10 @@ router.post('/monday/unsubscribe', authenticationMiddleware, triggerController.u
  * The endpoint to receive webhook events from Github. It is called when a new issue is created.
  * Each instance of the trigger block will use a unique subscription ID.
  * For implementation, see '/apps/github-node/src/services/github-service.js'
+ * 
+ * NOTE: You should authenticate incoming requests in a production app. 
+ * For an example, see `/apps/github-node/src/middlewares/authentication.js`
  */
-router.post('/integration/integration-events/:subscriptionId', triggerController.triggerEventsHandler);
+router.post('/integration/integration-events/:subscriptionId', triggerController.triggerEventsHandler); 
 
 export default router;
