8ed3daf50a69d0870f378a9efa8be6650ec0a592
Matthew Towarnicky <matthewto@monday.com>
2025-03-13 11:39:58 -0600

    Moved oauth to its own route to make simpler

--- apps/monday-code-fullstack-example/src/routes/index.ts
+++ apps/monday-code-fullstack-example/src/routes/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import mondayRoutes from './monday-route';
 import appEventsRoutes from './app-webhook-events';
+import outhRoutes from './oauth-route';
 const router = express.Router();
 
 // Add routes here, App.ts imports this file as the main router
 router.use(mondayRoutes);
 router.use(appEventsRoutes);
+router.use(outhRoutes);
 
 // serve client app
 router.use(express.static('client/build'));
--- apps/monday-code-fullstack-example/src/routes/monday-route.ts
+++ apps/monday-code-fullstack-example/src/routes/monday-route.ts
@@ -1,78 +1,18 @@
 import { Router } from 'express';
-import jwt from 'jsonwebtoken';
 import {
   clientAuth,
-  exchangeAuthorizationCodeForToken,
-  checkOAuthToken,
   authenticationMiddleware,
 } from '../middlewares/authentication';
-import logger from '../utils/logger';
 import {
   handleChangeColumnValue,
   handleGetColumnValue,
 } from '../controllers/monday-controller';
-import { getSecret } from '../utils/secrets-manager';
-import { EnvManager } from '../utils/env-manager';
-
-import { secureStorageDelete } from '../utils/secure-storage';
 
 const router = Router();
 
-router.post('/api/monday/oauth-flow', clientAuth, checkOAuthToken);
-
-router.get('/oauth/callback', async (req, res) => {
-  const { code, state: token } = req.query;
-
-  const clientSecret = getSecret('CLIENT_SECRET');
-  const decodedToken = jwt.verify(token as string, clientSecret as string);
-  const { user_id, account_id, slug } = (decodedToken as jwt.JwtPayload)
-    .dat as {
-    account_id: string;
-    user_id: string;
-    slug: string;
-  };
-
-  try {
-    // Exchange authorization code for access token here
-    await exchangeAuthorizationCodeForToken(
-      code,
-      user_id,
-      account_id,
-      slug,
-      clientSecret as string
-    );
-
-    // After successful token retrieval, redirect the user to the main screen
-
-    res.redirect(`${new EnvManager().get('LIVE_URL')}`);
-  } catch (error) {
-    // Handle error cases
-    res.status(500).json({ success: false, error: (error as Error).message });
-  }
-});
-
-//get LIVE URL for the FE
-router.get('/live-url', async (req, res) => {
-  res.status(200).send({ url: new EnvManager().get('LIVE_URL') });
-});
-
-if (process.env.NODE_ENV === 'development') {
-  // Uninstall endpoint to delete oauth token only for testing purposes
-  router.post('/delete-oauth-token', async (req, res) => {
-    const { account_id } = req.body;
-    try {
-      await secureStorageDelete(`${account_id}-token`);
-      return res.status(200).send({ success: true });
-    } catch (err) {
-      logger.error((err as Error).message);
-      return res.status(500).send({ message: 'Error deleting Oauth token' });
-    }
-  });
-}
-
 // Example routes
 // These are just examples and can be removed clientAuth is used to check if the request is coming from the client
-// authenticationMiddleware is used to check if the request is coming from a monday board
+// authenticationMiddleware is used to check if the request is coming from a monday board / integration
 router.post('/get-column-value', clientAuth, handleGetColumnValue);
 
 router.post(
