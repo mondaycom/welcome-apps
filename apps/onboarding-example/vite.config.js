8a89cb24300abe225947ad7e3a0eb3c301af3f6b
Dipro B <dipro@monday.com>
2025-03-18 16:14:06 -0400

    onboarding example: move to vite

---
+++ apps/onboarding-example/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: 'build',
+    },
+    plugins: [react()],
+    server: {
+      port: 8301,
+      allowedHosts: ['.apps-tunnel.monday.app']
+    }
+  };
+});
