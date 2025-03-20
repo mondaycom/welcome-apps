751249c84995acbce3eec87c162518ffdd15098d
Matthew Towarnicky <matthewto@monday.com>
2025-03-20 13:23:32 -0600

    Update vite.config.js

--- apps/monday-code-fullstack-example/client/vite.config.js
+++ apps/monday-code-fullstack-example/client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
+      '/': {
         target: 'http://localhost:8080',
         changeOrigin: true,
       }
